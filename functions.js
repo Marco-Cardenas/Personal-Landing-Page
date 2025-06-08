9e45dab67a1605d17074ebfc0c6429a93bd1488a
Marco Cárdenas <120759277+Marco-Cardenas@users.noreply.github.com>
2025-06-08 12:25:39 -0400

    Probando el css, js en el servidor

---
+++ functions.js
@@ -0,0 +1,4 @@
+document.getElementById("bHolaMundo").addEventListener("click", function() {
+  document.getElementById("holaMundo").innerHTML = "Hello, World";
+  document.getElementById("holaMundo").style.color = "red";
+});
